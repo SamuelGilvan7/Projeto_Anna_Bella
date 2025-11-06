9e058669aaa598283ef8fcccdbca05b813e94fad
SamuelGilvan7 <sankiragilvan@gmail.com>
2025-11-05 22:27:39 -0300

    Criei 6 arquivos
    Foi criado 6 arquivos(biografia , campanhas publicitarias , contato, header, index, style.css).

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+    <img src="img/capa.png" alt="Imagem da modelo Anna Bella" srcset="">
+    <nav>
+        <ul>
+            <li><a href="#">HOME</a></li> |
+            <li><a href="biografia.php">BIOGRAFIA</a></li> |
+            <li><a href="campanhas_publicitarias.php">CAMPANHAS PUBLICITÁRIAS</a></li> |
+            <li><a href="contato.php">CONTATO</a></li> 
+        </ul>
+    </nav>
+</header>
